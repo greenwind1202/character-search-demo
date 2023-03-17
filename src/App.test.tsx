4e085756cc926791eb76d750a836471e0b3d4fef
PhongPham <phongptt1202@gmail.com>
2023-03-17 11:52:37 +0700

    Config to run app on docker

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        results: [
+          {
+            name: 'Luke Skywalker',
+            height: '172',
+            mass: '77',
+            hair_color: 'blond',
+            skin_color: 'fair',
+            eye_color: 'blue',
+            birth_year: '19BBY',
+            gender: 'male',
+          },
+        ],
+      }),
+  })
+) as jest.Mock;
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SEARCHING CHARACTER APPLICATION/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe('App', () => {
+  it('load character on mount', async () => {
+    await act(async () => render(<App />));
+    fireEvent.click(screen.getByRole('button'));
+    expect(screen.getByText('Luke Skywalker')).toBeInTheDocument();
+  });
 });
